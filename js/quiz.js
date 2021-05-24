727fde5f635b598365003715d400d523ef971ffb
AromaWebSolution <34625501+AromaWebSolution@users.noreply.github.com>
2021-05-24 09:44:32 +0530

    Added login functionality for quiz test

--- js/quiz.js
+++ js/quiz.js
@@ -22,6 +22,40 @@ var quizControllerQuiz = (function () {
       return JSON.parse(localStorage.getItem('userName'));
     }
   };
+    
+   return {
+    getQuestionFromQuizPage: questionLocalStorage,
+    removeInputError: function(focusElement, errorMessage) {
+      focusElement.addEventListener('keydown', function() {
+        errorMessage.style.display = "none";
+      });
+    },   
+    checkLogin: function(fstName, lstName) {
+
+    var inputErrorFirstname = document.querySelector('.input-error-firstname');
+    var inputErrorLastname = document.querySelector('.input-error-lastname');
+
+      if(fstName.value === ''){
+        inputErrorFirstname.style.display = "block";
+        this.removeInputError(fstName, inputErrorFirstname); 
+      }
+      if (lstName.value === '') {
+        inputErrorLastname.style.display = "block";
+        this.removeInputError(lstName, inputErrorLastname);  
+      }
+      else {
+          var firstName = fstName.value;
+          var lastName = lstName.value;
+          var name = firstName + lastName;
+          var userName = new User(firstName, lastName, 0, 0);
+          questionLocalStorage.setUserOnLocalStorage(userName);
+          console.log(firstName, lastName);
+          fstName.value = '';
+          lstName.value = '';
+          document.querySelector('#login-btn').href = "quiz.html";
+      }
+    },
+   };
 })();
 
 /*****************************/
@@ -29,6 +63,17 @@ var quizControllerQuiz = (function () {
 /*****************************/
 
 var UIController = (function() {
+    
+   var domItems = {
+    firstName: document.getElementById('first-name'),
+    lastName: document.getElementById('last-name'),
+    loginButton: document.getElementById('login-btn')
+	};
+    
+	return {
+		getDomItems: domItems,
+    };
+    
 })();
 
 /*****************************/
@@ -36,4 +81,17 @@ var UIController = (function() {
 /*****************************/
 
 var moduleController = (function(quizctrl, uictrl) {
+   var selectedDomItems = uictrl.getDomItems;
+
+if(selectedDomItems.loginButton) {
+  selectedDomItems.loginButton.addEventListener('click', function(event) {
+    quizctrl.checkLogin(selectedDomItems.firstName, selectedDomItems.lastName);
+  });
+}
+
+  document.addEventListener('keydown', function(event) {
+      if (event.keyCode === 13 || event.which === 13) {
+           quizctrl.checkLogin(selectedDomItems.firstName, selectedDomItems.lastName);
+        }
+   });
 })(quizControllerQuiz, UIController);
