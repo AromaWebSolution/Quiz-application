0c132dbf3efc6bb84d33c9489fd83e02e131c46b
AromaWebSolution <rehanaroma786@gmail.com>
2021-06-17 08:49:57 +0530

    made chanages in add question and update question

--- js/script.js
+++ js/script.js
@@ -10,29 +10,10 @@ var quizController = (function () {
   	this.options = options;
   	this.correctAnswer = correctAnswer;
   }
-	
-  // var questionLocalStorage = {
-  //   setQuestionCollection: function(newCollection) {
-  //     localStorage.setItem('QuestionId', JSON.stringify(newCollection));
-  //   },
-  //   getQuestionCollection: function() { 
-  //    return JSON.parse(localStorage.getItem('QuestionId'));
-  //   },
-  //   removeQuestionCollection: function() {
-  //     localStorage.removeItem('QuestionId');
-  //   },
-  //   clearLocaleStorage: function() {
-  //     localStorage.clear('QuestionId');
-  //   }
-  // };
-  // if (questionLocalStorage.getQuestionCollection() === null) {
-  //       questionLocalStorage.setQuestionCollection([]);
-  // }
- 
+	 
 return {
-  // getQuestionLocalStorage: questionLocalStorage,
-  addQuestionOnLocalStorage: function(newQuestText, opts) {
-     var optionsArr, corrAns, questionId, newQuestion, storedQuest, isChecked;
+  addQuestionOnDatabse: function(newQuestText, opts) {
+     var optionsArr, corrAns, newQuestion, isChecked;
       optionsArr = [];
       isChecked = false;
  
@@ -46,20 +27,11 @@ return {
         }
       }
 
-      // if (questionLocalStorage.getQuestionCollection().length > 0) {
-      //  questionId = questionLocalStorage.getQuestionCollection()[questionLocalStorage.getQuestionCollection().length -1].id + 1;
-      // }
-      // else{
-      //   questionId = 0;
-      // }
       if (newQuestText.value !== '') {
         if (optionsArr.length > 1) {
           if (isChecked) {
-      newQuestion = new Question(questionId, newQuestText.value, optionsArr, corrAns);
+      newQuestion = new Question(newQuestText.value, optionsArr, corrAns);
       
-      // storedQuest = questionLocalStorage.getQuestionCollection();
-      // storedQuest.push(newQuestion);
-      // questionLocalStorage.setQuestionCollection(storedQuest);
       axios.post('https://quiz-application-ca0b3-default-rtdb.firebaseio.com/questions.json', newQuestion)
       .then(response => console.log(response))
       .catch(error => console.log(error));
@@ -166,7 +138,17 @@ var domItems = {
 
       domItems.insertedQuestionWrapper.innerHTML = '';
       questionNumber = 0;
-      axios.get('https://quiz-application-ca0b3-default-rtdb.firebaseio.com/questions.json')
+      function escapeHtml(text) {
+      var map = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#039;'
+        };
+        return text.replace(/[&<>"']/g, function(m) { return map[m]; });
+        }
+        axios.get('https://quiz-application-ca0b3-default-rtdb.firebaseio.com/questions.json')
         .then(response => {
           const users = [];
           const data = response.data;
@@ -175,26 +157,20 @@ var domItems = {
             user.id = key;
             users.push(user);
           }
-          console.log(users);
           for(var i = 0; i < users.length; i++) {
-        questHTML = '<p id="' + users[i].id + '"><span class="inserted-ques-text">' + ++questionNumber + '- ' + users[i].questionText + '</span><button class="edit-btn edit-btn-' + users.indexOf(users[i]) + '">Edit</button></p>';
+        questHTML = '<p id="' + users[i].id + '"><span class="inserted-ques-text">' + ++questionNumber + '- ' + escapeHtml(users[i].questionText) + '</span><button class="edit-btn edit-btn-' + users.indexOf(users[i]) + '">Edit</button></p>';
         domItems.insertedQuestionWrapper.insertAdjacentHTML('beforeend', questHTML);
-          } 
+          }
         })
-        // questionNumber++;
         .catch(error => console.log(error));
-      
-      // for (var i = 0; i < getQuestions.getQuestionCollection().length; i++) {
-      // }
-      
-    },
+      },
     addInput: function() {
           var inputHTML, z;
           z = document.querySelectorAll('.admin-option').length;
           inputHTML = '<div class="admin-options-wrapper"><input type="radio" name="answer" class="admin-options-' + z + '" value="' + z + '"><input type="text" class="admin-option admin-options-' + z + '" value=""></div>';
           domItems.adminOptionsContainer.insertAdjacentHTML('beforeend', inputHTML);
       },
-       editQuestionList: function(event, getEditQuestion) {
+    editQuestionList: function(event, getEditQuestion) {
       var itemID, optionsList, z;
       itemID = event.target.parentNode.id;
       let adminOptsWrapper = document.querySelectorAll('.admin-options-wrapper');
@@ -275,26 +251,19 @@ var domItems = {
     domItems.deleteButton.style.display = 'inline-block';
     domItems.updateButton.style.display = 'inline-block';
 },
-    updateQuestion: function() {
-      let mylStorage = quizController.getQuestionLocalStorage.getQuestionCollection();
+  updateQuestion: function() {
       let adminOpts = document.querySelectorAll('.admin-option');
       let newQuestText  = document.getElementById('new-question-text');
 
-    function Question(id, questionText, options, correctAnswer) {
-    this.id = id;
+    function Question(questionText, options, correctAnswer) {
     this.questionText = questionText;
     this.options = options;
     this.correctAnswer = correctAnswer;
   }
-
-      for(var i = 0; i < mylStorage.length; i++) {
-        if (mylStorage[i].id === parseInt(domItems.formsWrapper.id)) {
-          var questionId = mylStorage[i].id;
-          var questionIndex = mylStorage.indexOf(mylStorage[i]);
     
-           function updateFunction(newQuestText, opts) {
-            var optionsArr, corrAns, newQuestion, storedQuest, isChecked;
-            optionsArr = [];
+    function updateFunction(newQuestText, opts) {
+      var optionsArr, corrAns, newQuestion, isChecked;
+      optionsArr = [];
  
       for (var i = 0; i < opts.length; i++) {
         if(opts[i].value !== "") {
@@ -309,9 +278,10 @@ var domItems = {
       if (newQuestText.value !== '') {
         if (optionsArr.length > 1) {
           if (isChecked) {
-            newQuestion = new Question(questionId, newQuestText.value, optionsArr, corrAns);
-              mylStorage.splice(questionIndex, 1, newQuestion);
-            quizController.getQuestionLocalStorage.setQuestionCollection(mylStorage);
+            newQuestion = new Question(newQuestText.value, optionsArr, corrAns);
+            axios.patch(`https://quiz-application-ca0b3-default-rtdb.firebaseio.com/questions/${domItems.formsWrapper.id}.json`, newQuestion)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
            newQuestText.value = '';
 
       for (var x = 0; x < opts.length; x++) {
@@ -334,8 +304,6 @@ var domItems = {
       }
       }
       updateFunction(newQuestText, adminOpts);
-        }
-      }
     },
     deleteQuestionUI: function() {
       var opts = document.querySelectorAll('.admin-option');
@@ -464,7 +432,7 @@ var moduleController = (function(quizctrl, uictrl) {
 
     selectedDomItems.questInsertBtn.addEventListener('click', function() {
         var adminOpts = document.querySelectorAll('.admin-option');
-        quizctrl.addQuestionOnLocalStorage(selectedDomItems.newQuestText, adminOpts);
+        quizctrl.addQuestionOnDatabse(selectedDomItems.newQuestText, adminOpts);
         uictrl.removeInputUI();
         uictrl.addInput();
         uictrl.addInput();
